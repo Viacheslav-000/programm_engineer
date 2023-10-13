2fb8f6b2881dc051f5b494f97f23aa3aade4ef4d
Viacheslav-000 <146506763+Viacheslav-000@users.noreply.github.com>
2023-10-13 20:58:55 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+# 1)
+print(123)
+print('123')
+print(1.23)
+
+# 2)
+print(123 + 795)
+print(24.95 + 13.34)
+print(14.08 - 5 - 3.14 - 2)
+
+# 3)
+print('Привет, Мир!')
+
+world = 'Мир'
+print(f"Привет, {world}!")
+
+one = 'Привет, '
+two = 'Мир!'
+
+print(one + two)
+
+#4)
+one = 'Hello'
+print(bool(one))
+
+two = '404'
+print(float(two))
+
+three = None
+print(str(three))
+
+#5)
+one = input('one: ')
+two = input('two: ')
+three = input('three: ')
+print(one, two, three)
+
+#6)
+a = 20
+b = 6
+print('Возведение в степень: ', a ** b)
+print('Обычное деление: ', a / b)
+print('Целочисленное деление: ', a // b)
+print('Нахождение остатка от деления: ', a % b)
+
+#7)
+line = 'Hello!'
+print(line*6)
+
+#8)
+sentence = "Hello World"
+print(sentence.count('o'))
+
+#9)
+print('Hello\nWorld')
+
+#10)
+sentence = 'Hello world'
+print(sentence[1])
+print(sentence[:5])
